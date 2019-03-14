f966893e13631a26371343e3c35ffed6d04f4813
Joshua Thornton <joshua.thornton@protonmail.com>
2019-03-14 09:08:54 -0400

    updating chain length

--- chain.go
+++ chain.go
@@ -21,11 +21,18 @@ type chain struct {
 	db DaterWriter
 }
 
-// MarkToday marks today as done in the database.
+// MarkToday marks today as done in the database, adding to the existing chain.
 func (c *chain) MarkToday() error {
 	t := today()
 	log.Debug().Msgf("marking today '%s' in database", t)
 	return c.db.WriteLastDate(t)
+}
+
+// Start will mark today as the first day in the chain.
+func (c *chain) Start() error {
+	t := today()
+	log.Debug().Msgf("starting chain with today '%s'", t)
+	return c.db.WriteFirstDate(t)
 }
 
 // Broken reports if the chain has been broken.
@@ -37,11 +44,24 @@ func (c *chain) Broken() bool {
 }
 
 // Length returns the number of days the chain has been unbroken for.
-func (c *chain) Length() (int, error) {
-	d, err := time.Parse(dateFmt, c.db.FirstDate()) // FIXME
+func (c *chain) Length() int {
+	first, err := time.Parse(dateFmt, c.db.FirstDate())
 	if err != nil {
-		return 0, err
+		// not sure we want a basic length func to return an error
+		// we should only see this error if the date format is incorrect
+		// or if the first date is wrong, which is written by this package
+		log.Fatal().Err(err)
 	}
-	hoursDiff := time.Now().Sub(d).Hours()
-	return int(hoursDiff / 24), nil
+	last, err := time.Parse(dateFmt, c.db.LastDate())
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+
+	hoursDiff := last.Sub(first).Hours()
+
+	// when starting the chain the first day and last will
+	// always be the same. we want to always add 1 day to
+	// the len to get an accurate reading while maintaining
+	// a common sense db format
+	return int(hoursDiff/24) + 1
 }
